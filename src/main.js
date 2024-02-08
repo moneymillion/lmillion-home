30405c47c9f59da7819b09221ea2c21b940f9b18
UBI-Solver <157436324+moneymillion@users.noreply.github.com>
2024-02-08 12:59:12 -0500

    v5.0

--- src/main.js
+++ src/main.js
@@ -3,12 +3,18 @@ import { Application } from '@splinetool/runtime';
 const canvas = document.getElementById('canvas3d');
 const app = new Application(canvas);
 app.load('https://prod.spline.design/cdrWwGdXIKuUW4RW/scene.splinecode').then(() => {
-  document.getElementById('get-started-btn').style.display = 'block';
+  console.log("Scene loaded");
+  setTimeout(() => {
+    document.getElementById('get-started-btn').style.display = 'block';
+    console.log("Button displayed");
+  }, 100); // Delay in ms, adjust as needed
 });
 
 document.addEventListener('DOMContentLoaded', function() {
     var getStartedBtn = document.getElementById('get-started-btn');
     getStartedBtn.addEventListener('click', function() {
         window.location.href = "https://lmillion.builder-preview.com/";
+        console.log("DOM fully loaded and parsed");
+
     });
 });
